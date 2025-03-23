3f9adb9e874855e723a17a00d58c66d27456f6aa
Polz2 <somarribayaser02@gmail.com>
2025-03-22 20:26:08 -0600

    Actualizacion10

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ import { cargarProductos } from "./Funciones/cargarProductos.js";
 import { agregarEvento } from "./Funciones/clickiProductoBox.js";
 import { agregarFuncionalidadCarrito } from "./Funciones/agregarFuncionalidadCarrito.js";
 
+alert(`SOlo FUnciona el CArro, agregas y quitas
+      Si aun no has agregado nada al carro no podes acceder al carro,
+      si ya agregaste si podes acceder al carrito,
+  .`);
+
+alert("Si sos Alexis Fuck you, si no igual");
 
 const carrito = document.querySelector(".carrito").classList.add("carrito-off");
 const arr = new Productos();
